a979b1f159de24726ecab91663b7583d7f1f692b
zhenwei.liu <zhenwei.liu@qunar.com>
2015-01-01 00:40:30 +0800

    first commit

--- swift-demo/main.swift
+++ swift-demo/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  swift-demo
-//
-//  Created by zhenwei.liu on 1/1/15.
-//  Copyright (c) 2015 test.zwl. All rights reserved.
-//
-
 import Foundation
 
 println("Hello, World!")
